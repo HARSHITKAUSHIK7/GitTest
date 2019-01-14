fbac5cea4049255ebce40f559a3f9240c46dbfa9
harshit <kaushikharshit98@gmail.com>
2019-01-14 09:05:10 -0800

    added

---
+++ program2/src/ascii.java
@@ -0,0 +1,11 @@
+
+public class ascii {
+	public static void main(String[]args)
+	{
+	char c= 'b';
+	int value= c;
+	System.out.println("Ascii:"+value);
+	
+	System.out.println("Ascii :"+(int)c);
+	}
+}
